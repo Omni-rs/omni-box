5f3e4e45b350ac909a5148dc604c40287fb54e2b
edsonalcala <edson_alcala@msn.com>
2024-12-11 14:35:53 +0000

    remove unused imports

--- src/clients/anvil.rs
+++ src/clients/anvil.rs
@@ -1,8 +1,5 @@
+use alloy::node_bindings::Anvil;
 use alloy::node_bindings::AnvilInstance;
-use alloy::{
-    network::EthereumWallet, node_bindings::Anvil, providers::ProviderBuilder,
-    signers::local::PrivateKeySigner,
-};
 
 pub fn get_anvil_instance() -> Result<AnvilInstance, Box<dyn std::error::Error>> {
     // Spin up a local Anvil node.
