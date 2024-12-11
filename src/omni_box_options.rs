201f7eea45340237d3658ca6f9cf8f31a24993cf
edsonalcala <edson_alcala@msn.com>
2024-12-11 14:37:05 +0000

    extended near options

--- src/omni_box_options.rs
+++ src/omni_box_options.rs
@@ -1,10 +1,12 @@
-use crate::{chain_config::ChainOverrides, network::Network};
+use crate::{chain_config::ChainOverrides, network::Network, NearNetworkConfig};
 use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 pub struct OmniBoxOptions {
     pub modules: Vec<Network>,                       // Networks to include
     pub overrides: HashMap<Network, ChainOverrides>, // Overrides for each network
+    pub path: &'static str,                          // Path to the config file
+    pub default_near_network: NearNetworkConfig,     // Default Near network
 }
 
 impl Default for OmniBoxOptions {
@@ -12,6 +14,8 @@ impl Default for OmniBoxOptions {
         Self {
             modules: vec![Network::Ethereum, Network::Near, Network::Bitcoin],
             overrides: HashMap::new(),
+            path: "./",
+            default_near_network: NearNetworkConfig::Testnet,
         }
     }
 }
